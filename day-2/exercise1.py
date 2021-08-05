a9f3008c129c45fd68aa98d78190d3312c4d2449
Sundar Bishnoi <sundarlal2929@gmail.com>
2021-08-05 13:12:32 +0530

    Strings, Variables, and Getting Input from Users

---
+++ day-2/exercise1.py
@@ -0,0 +1,4 @@
+name = input("your name: ")
+age =  input("your age: ")
+print('Name: ' + name)
+print('Age: ' + age)
---
+++ day-2/exercise3.py
@@ -0,0 +1,8 @@
+hourly_wage = input("Please enter your hourly wage: ")
+
+print("Hourly wage: ")
+print(hourly_wage)
+print("Hours worked: ")
+
+hours_worked = input("How many hours did you work this week? ")
+print(hours_worked)
