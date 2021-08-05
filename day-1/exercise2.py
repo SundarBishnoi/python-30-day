ae3d0700c96b8aa9843bb90bef9b3b6b12d42377
Sundar Bishnoi <sundarlal2929@gmail.com>
2021-08-05 07:36:59 +0530

    day 1 exercises

---
+++ day-1/exercise2.py
@@ -0,0 +1,10 @@
+no_of_days = 365
+no_of_weeks = 52
+no_of_months = 12
+years = 27
+total_days = years*no_of_days
+total_months = years*no_of_months
+total_weeks = years*no_of_weeks
+print(total_days)
+print(total_months)
+print(total_weeks)
---
+++ day-1/exercise3.py
@@ -0,0 +1,3 @@
+pi = 3.14
+radius = 5
+print(pi*pow(radius,2))
