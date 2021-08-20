faf5711bca0f284edfb1eb5e5940e3d29874d25c
Sundar Bishnoi <sundarlal2929@gmail.com>
2021-08-20 08:12:22 +0530

    Create practice.py
    ok

---
+++ day-3/practice.py
@@ -0,0 +1,5 @@
+output  = "{} is {} years old, and works as a {}."
+print(output.format("John", 24, "SRE"))
+
+output = "{0} is {1} years old, and {0} works as a {2}."
+print(output.format("John", 24, "SRE"))
